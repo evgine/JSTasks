971cc5a073bf4d46dbbe0a5a1d668b756425e8d6
yevginemnatsakanyan <evgine.v.mnatsakanyan@gmail.com>
2022-08-11 16:15:18 +0400

    Create mergeArr.js

---
+++ mergeArr.js
@@ -0,0 +1,21 @@
+function merge(arr1, arr2, arr3, arr4, arr5) {
+    let arr = [...arr1, ...arr2, ...arr3, ...arr4, ...arr5]
+    let arrMap = new Map();
+    let newArr = [];
+    for (let i = 0; i <= 10; i++) {
+        arrMap.set(i, 0)
+    }
+    for (let itemArr of arr) {
+        arrMap.forEach(function (value, key) {
+            if (itemArr === key) {
+                arrMap.set(key, value + 1)
+            }
+        })
+    }
+    arrMap.forEach(function (value, key) {
+        for (let i = 1; i <= value; i++) {
+            newArr.push(key);
+        }
+    })
+    return newArr
+}
