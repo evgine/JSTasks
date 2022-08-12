1ff338ecfc095ccfdb2017971e4e150970b538c5
yevginemnatsakanyan <evgine.v.mnatsakanyan@gmail.com>
2022-08-12 17:26:14 +0400

    Update mergeArr.js

--- mergeArr.js
+++ mergeArr.js
@@ -1,3 +1,5 @@
+//The first bad sversion
+
 // function merge(arr1, arr2, arr3, arr4, arr5) {
     
 //     let arr = [...arr1, ...arr2, ...arr3, ...arr4, ...arr5];
@@ -22,50 +24,82 @@
 //     return newArr;
 // }
 
-function merge(arr1, arr2, arr3, arr4, arr5) {
-    let arr = [arr1[0], arr2[0], arr3[0], arr4[0], arr5[0]];
-    let longest = arr1.length + arr2.length + arr3.length + arr4.length + arr5.length;
-    let newArr = [];
+//The second bad version
 
-    let indexOfOne = 0, indexOfTwo = 0, indexOfThree = 0, indexOfFour = 0, indexOfFive = 0;
+// function merge(arr1, arr2, arr3, arr4, arr5) {
+//     let arr = [arr1[0], arr2[0], arr3[0], arr4[0], arr5[0]];
+//     let longest = arr1.length + arr2.length + arr3.length + arr4.length + arr5.length;
+//     let newArr = [];
 
-    for (let i = 0; i < longest; i++) {
-        newArr.push(Math.min(...arr));
-        if (Math.min(...arr) === arr[0]) {
-            arr[0] = arr1[indexOfOne + 1];
-            indexOfOne++;
-            if (arr1[indexOfOne] == undefined) {
-                arr[0] = Infinity;
-            }
+//     let indexOfOne = 0, indexOfTwo = 0, indexOfThree = 0, indexOfFour = 0, indexOfFive = 0;
+
+//     for (let i = 0; i < longest; i++) {
+//         newArr.push(Math.min(...arr));
+//         if (Math.min(...arr) === arr[0]) {
+//             arr[0] = arr1[indexOfOne + 1];
+//             indexOfOne++;
+//             if (arr1[indexOfOne] == undefined) {
+//                 arr[0] = Infinity;
+//             }
+//         }
+//         else if (Math.min(...arr) === arr[1]) {
+//             arr[1] = arr2[indexOfTwo + 1];
+//             indexOfTwo++;
+//             if (arr2[indexOfTwo] == undefined) {
+//                 arr[1] = Infinity;
+//             }
+//         }
+//         else if (Math.min(...arr) === arr[2]) {
+//             arr[2] = arr3[indexOfThree + 1];
+//             indexOfThree++;
+//             if (arr3[indexOfThree] == undefined) {
+//                 arr[2] = Infinity;
+//             }
+//         }
+//         else if (Math.min(...arr) === arr[3]) {
+//             arr[3] = arr4[indexOfFour + 1];
+//             indexOfFour++;
+//             if (arr4[indexOfFour] == undefined) {
+//                 arr[3] = Infinity;
+//             }
+//         }
+//         else if (Math.min(...arr) === arr[4]) {
+//             arr[4] = arr5[indexOfFive + 1];
+//             indexOfFive++;
+//             if (arr5[indexOfFive] == undefined) {
+//                 arr[4] = Infinity;
+//             }
+//         }
+//     }
+//     return newArr;
+// }
+
+
+
+// The last version(bad or good:))))
+
+function finalMerge(arr1, arr2, arr3, arr4, arr5) {
+    function mergeArrs(arr1, arr2) {
+
+        let lengthOfArr1 = arr1.length;
+        let lengthOfArr2 = arr2.length;
+
+        let newArr = Array(lengthOfArr1 + lengthOfArr2).fill(0);
+        let i = 0, j = 0, k = 0;
+
+        while (i < lengthOfArr1 && j < lengthOfArr2) {
+            if (arr1[i] < arr2[j])
+                newArr[k++] = arr1[i++];
+            else
+                newArr[k++] = arr2[j++];
         }
-        else if (Math.min(...arr) === arr[1]) {
-            arr[1] = arr2[indexOfTwo + 1];
-            indexOfTwo++;
-            if (arr2[indexOfTwo] == undefined) {
-                arr[1] = Infinity;
-            }
+        while (i < lengthOfArr1) {
+            newArr[k++] = arr1[i++];
         }
-        else if (Math.min(...arr) === arr[2]) {
-            arr[2] = arr3[indexOfThree + 1];
-            indexOfThree++;
-            if (arr3[indexOfThree] == undefined) {
-                arr[2] = Infinity;
-            }
+        while (j < lengthOfArr2) {
+            newArr[k++] = arr2[j++];
         }
-        else if (Math.min(...arr) === arr[3]) {
-            arr[3] = arr4[indexOfFour + 1];
-            indexOfFour++;
-            if (arr4[indexOfFour] == undefined) {
-                arr[3] = Infinity;
-            }
-        }
-        else if (Math.min(...arr) === arr[4]) {
-            arr[4] = arr5[indexOfFive + 1];
-            indexOfFive++;
-            if (arr5[indexOfFive] == undefined) {
-                arr[4] = Infinity;
-            }
-        }
+        return newArr;
     }
-    return newArr;
+    return mergeArrs(mergeArrs(mergeArrs(mergeArrs(arr1, arr2), arr3), arr4), arr5)
 }
