2d6e5459ad87ba2d0fc015992812cea99c3a32da
yevginemnatsakanyan <evgine.v.mnatsakanyan@gmail.com>
2022-08-14 13:54:46 +0400

    Update isUnique.js

--- isUnique.js
+++ isUnique.js
@@ -1,1 +1,13 @@
+function isUnique(str){
+    let obj = {};
+    for (let char in str) {
+        if (obj.hasOwnProperty(str.codePointAt(char))) {
+            return "String isn't unique";
+        } else {
+            obj[str.codePointAt(char)] = 1;
+        }
+    }
+    return "String is unique"
+}
 
+//NOTE:
